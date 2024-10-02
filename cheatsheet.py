5e52d186f06b7516b03833ad6249466013a64828
Robert Bendun <robert@bendun.cc>
2024-10-02 09:17:44 +0200

    robots go away

--- cheatsheet.py
+++ cheatsheet.py
@@ -54,6 +54,7 @@ else:
 	<head>
 		<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="robots" content="noindex,nofollow,nosnippet,noodp,noarchive,noimageindex">
 		<title>Python - Ruby cheatsheet</title>
         <style>
         html {{
