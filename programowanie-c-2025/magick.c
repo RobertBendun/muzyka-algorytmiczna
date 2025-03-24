dbeb166cdb1bdb2798b9782b59c24bf5ee5f2de8
Robert Bendun <robert@bendun.cc>
2025-03-24 18:55:22 +0100

    05

--- programowanie-c-2025/magick.c
+++ programowanie-c-2025/magick.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <arpa/inet.h>
 
 /*
     ./magick input.ppm -flip output.ppm
+    ./magick -generate-color [color] output.ppm
 */
 
 void save_ppm(uint32_t *image, size_t width, size_t height, FILE *out)
@@ -24,23 +27,36 @@ void save_ppm(uint32_t *image, size_t width, size_t height, FILE *out)
 // string[] argument value
 int main(int argc, char* argv[])
 {
-    if (argc < 2) {
-        printf("%s: error: no input file\n", argv[0]);
-        return 1;
-    }
-    char const *input_file_path = argv[1];
+    char const *input_file_path = NULL;
+    char const *flag = NULL;
+    char const *output_file_path = NULL;
+    char const *generate_color = NULL;
 
-    if (argc < 3) {
-        printf("%s: error: no flags\n", argv[0]);
-        return 1;
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp("-generate-color", argv[i]) == 0) {
+            if (i+1 >= argc) {
+                printf("%s: error: -generate-color expects color", argv[0]);
+                return 1;
+            }
+            generate_color = argv[i+1];
+            i++;
+            continue;
+        } else if (argv[i][0] == '-') {
+            printf("%s: error: unknown flag: %s\n", argv[0], argv[i]);
+            return 1;
+        } else if (input_file_path == NULL) {
+            input_file_path = argv[i];
+        } else if (output_file_path == NULL) {
+            output_file_path = argv[i];
+        } else {
+            printf("%s: error: no more positionals were expected\n", argv[i]);
+            return 1;
+        }
     }
-    char const *flag = argv[2];
 
-    if (argc < 4) {
-        printf("%s: error: no output file\n", argv[0]);
-        return 1;
+    if (generate_color != NULL) {
+        output_file_path = input_file_path;
     }
-    char const *output_file_path = argv[3];
 
     printf("in = %s\n", input_file_path);
     printf("out = %s\n", output_file_path);
@@ -63,9 +79,15 @@ int main(int argc, char* argv[])
     size_t height = 32;
     uint32_t *image = malloc(sizeof(int) * width * height);
 
+    uint32_t color;
+
+    // TODO: Documentation how we got to this mess will be added
+    sscanf(generate_color, "%x", &color);
+
     for (int i = 0; i < width; ++i) {
         for (int j = 0; j < height; ++j) {
-            image[i * width + j] = 0x0000ff;
+            // color must be provided in little endian
+            image[i * width + j] = ntohl(color) >> 8;
         }
     }
 
